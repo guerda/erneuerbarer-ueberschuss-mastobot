8160599a531a9255e8333338c3d0876bfea3da91
Philip Gillißen <guerda@freenet.de>
2026-02-16 22:03:39 +0100

    style: formatting

--- test_mastodon_connection.py
+++ test_mastodon_connection.py
@@ -6,9 +6,6 @@ import os
 def test_mastodon_client():
     dotenv.load_dotenv()
     token = os.getenv("ACCESS_TOKEN")
-    assert token is not None 
-    mastodon = Mastodon(
-        api_base_url="https://ruhr.social", access_token=token
-
-    )
+    assert token is not None
+    mastodon = Mastodon(api_base_url="https://ruhr.social", access_token=token)
     assert mastodon.me() is not None
